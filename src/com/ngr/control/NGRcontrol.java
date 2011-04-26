68bec29604001e8aeb680ae3986c5f99e9ef214b
Cory M <HiVoltList@gmail.com>
2011-04-26 15:38:33 -0400

    More work on main for connecting

--- src/com/ngr/control/NGRcontrol.java
+++ src/com/ngr/control/NGRcontrol.java
@@ -4,12 +4,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.LinkedList;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -18,9 +16,9 @@ import android.widget.EditText;
 public class NGRcontrol extends Activity 
 {
 
+	LinkedList<String> commandQ = new LinkedList<String>();
 	
-    /** Called when the activity is first created. */
-	
+    //Called when the activity is first created
     @Override
     public void onCreate(Bundle savedInstanceState) 
     {
@@ -36,14 +34,58 @@ public class NGRcontrol extends Activity
     	inputText.setText("");
     }
     
-    public void connectTelnet()
+    //handles main controls for car operation
+    public void main()
     {    	
     	final String inputText = (String)findViewById(R.id.inputText).toString();
+    	PrintWriter out = openConnection(inputText);
     	
+    	if(out != null)
+    	{
+    		while(true)
+    		{
+    			if(!commandQ.isEmpty())
+    			{
+        			try 
+        			{
+    					Thread.sleep(20L);
+    				} 
+        			catch (InterruptedException e) 
+        			{
+        				AlertDialog alert = new AlertDialog.Builder(this).create();
+    					alert.setTitle("Exception!");
+    					alert.setMessage("It seems you have an Interrupted Exception. main()");
+    					e.printStackTrace();
+    				}
+        			
+        			out.println("Element in command queue: " + commandQ.poll());
+        			out.flush();
+        			try
+        			{
+        				Thread.sleep(20L);
+        			}
+        			catch(InterruptedException e)
+        			{
+        				AlertDialog alert = new AlertDialog.Builder(this).create();
+        				alert.setTitle("Exception!");
+    					alert.setMessage("It seems you have an Interrupted Exception. main()");
+    					e.printStackTrace();
+        			}
+    			}		
+
+    		}
+    	}
+    	else
+    	{
+    		AlertDialog alert = new AlertDialog.Builder(this).create();
+    		alert.setTitle("Problem");
+			alert.setMessage("Either the car IP is invalid, or car not connected");
+    		System.exit(0);
+    	}
     	
     }
     
-	public static PrintWriter openConnection(String RCIP) {
+	public PrintWriter openConnection(String RCIP) {
 		Socket s;
 		PrintWriter out = null;
 		
@@ -54,12 +96,17 @@ public class NGRcontrol extends Activity
         } 
 		catch (UnknownHostException e) 
 		{
-            System.err.println("Cannot connect to or cannot find host.");
+			AlertDialog alert = new AlertDialog.Builder(this).create();
+			alert.setTitle("Exception!");
+			alert.setMessage("Cannot connect or cannot find host. openConnection()");
+			e.printStackTrace();
             System.exit(1);
         } 
 		catch (IOException e) 
 		{
-			System.out.println();
+			AlertDialog alert = new AlertDialog.Builder(this).create();
+			alert.setTitle("Exception!");
+			alert.setMessage("Looks like you found an I/O Exception. openConnection()");
 			e.printStackTrace();
 		}
         return out;
